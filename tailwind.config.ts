2b5cc8453400fe878bacdf2b63cd0fbd011876a9
yesha-aiplanet <yesha@aiplanet.com>
2025-06-20 16:08:07 +0530

    Toast remove delay decreased and animation changed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,7 +88,13 @@ export default {
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out'
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				'in': 'in 0.2s ease-out',
+				'out': 'out 0.2s ease-out',
+				'fade-in': 'fade-in 0.2s ease-out',
+				'fade-out': 'fade-out 0.2s ease-out',
+				'slide-in-from-top-full': 'slide-in-from-top-full 0.2s ease-out',
+				'slide-out-to-right-full': 'slide-out-to-right-full 0.2s ease-out',
 			}
 		}
 	},
